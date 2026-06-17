5d0d36875edc8844f1fe0261c8a2012eac3d0605
Lydia  Jepleting <lydiajepleting9@gmail.com>
2026-06-17 15:44:38 +0300

    learned primitive data string in javascript

---
+++ JS/Intro/primitiveDataType.js
@@ -0,0 +1,46 @@
+let number1= 34; //real number
+console .log(number1);
+let number2 =34.34; //floating point number
+console .log(number2);
+let number3 =-34 // negative number
+console .log(number3);
+//biggest number 1.7976931348623157 *10'308
+//3 way of representing string 
+//doubles quotes
+//single quotes
+//back ticks 
+let str1="iam lydia jepleting" //double
+console.log(str1);
+let str2 ='single quotes iam lydia jepleting '//single quotes
+console.log(str2);
+let str3=`back  ticks iam jepleting ` //back ticks
+console.log(str3);
+let str4 ="24"; //string or a number <string>
+console.log(str4);
+let str5 ='lydia said    "23 is a number"'
+let str6=`
+multi line string 
+" "   ''both single and double quotes `
+let str7='Don\'t  do this any more '
+
+
+/*  Boolean represent true or false */
+let johnIsMale=true // true value
+console.log (johnIsMale)
+let JohnIsMarried=false
+console.log(JohnIsMarried)
+
+
+/* null is lack of value 
+ie absences of value */
+let nullval1=null
+console.log(nullval1)
+/*undefined..> lack of value ie absences of a value 
+what btw null and undefined is ...>never explicity  set something  to undefined 
+if you want to create a variable  that does not have  a value never use undefined ,use null*/
+
+let undefinedval1=undefined //dont  do this 
+console.log(undefinedval1)// undefined
+let undefinedval2;
+console.log(undefinedval2)
+
