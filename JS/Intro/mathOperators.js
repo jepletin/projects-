f57c7ba0d736d7f09c27f15abc107ef3da44b01d
Lydia  Jepleting <lydiajepleting9@gmail.com>
2026-06-18 16:28:19 +0300

    learning math operators in javascript

---
+++ JS/Intro/mathOperators.js
@@ -0,0 +1,26 @@
+//Assignment operator
+let numb1=23; // declared and assigned the variable
+console.log(`numb1 is ${numb1} its type is ${typeof numb1}`)
+numb1=40 /*assignment only */
+console.log(`numb1   ${numb1} its type is ${typeof numb1}`)
+ let numb2 =60
+ console.log( `numb2 ${numb2} its type is ${typeof numb2}`)
+ let numb3=numb1+ numb2
+ console.log(`numb3 is ${numb3}  its type is ${typeof numb3}`)
+ let numb4= numb1*numb2
+ console.log(`numb4 is ${numb4} its type is ${numb4}`)
+let numb5=numb1**3
+console.log(`numb5 is ${numb5} its type is ${numb5}`)
+let numb6=Math.pow (10,3)//2^6
+console.log(`numb6 is ${numb6}its type is ${typeof numb6}`)
+
+
+let numb7 =30
+let numb8=10 
+let divNumb=numb7/numb8
+console.log(`divNumb ${divNumb} its type is ${typeof divNumb}`)
+//module is the remainder 
+let numb9 = 23
+let numb10 =12
+let modNumb=numb9%numb10
+console.log(`modNumb ${modNumb} its type is ${typeof modNumb}`)
