a092330f7ddfeac66a5a3a4a4f1662a91f154933
Lydia  Jepleting <lydiajepleting9@gmail.com>
2026-06-22 10:07:57 +0300

    completed comparison operatore

---
+++ JS/Intro/comparison.js
@@ -0,0 +1,61 @@
+/*comparison operators
+1  strict equality ===
+2 equality==
+3inequality !=
+strict inequality!==*/
+
+let numb1="67"
+let numb2=67
+let strictEqual=numb1===numb2
+console.log(`numb1=${numb1}===numb2${numb2}Ans ${strictEqual} its type ${typeof strictEqual}`)
+let nonStrictEquality=numb1==numb2
+console.log(`numb1=${numb1}==numb2=${numb2}Ans ${nonStrictEquality}its type ${typeof nonStrictEquality}`)
+
+
+
+
+let val1="hi"
+let val2= `lydia`
+let strictInequality=val1!==val2
+console.log(`val1=${val1}!==val2${val2}Ans ${strictInequality} its type ${typeof strictInequality}`)
+let inequality=val1!=numb2
+console.log(`val1=${val1}!=val2=${val2}Ans ${inequality}its type ${typeof inequality}`)
+
+
+
+
+
+
+
+
+
+let numb3=20;
+let numb4=10;
+
+
+let str1="a";
+let str2="b";
+
+console.log(`numb3=${numb3} and numb4 ${numb4}`)
+console.log(`str1=${str1} and str2=${str2}`)
+
+console.log(`numb3>numb4 ${ numb3>numb4}`)
+console.log(`str2>str1  ${str1>str2}`)
+
+console.log(`numb3>=numb4 ${ numb3>=numb4}`)
+console.log(`str2>=str1  ${str1>=str2}`)
+
+console.log(`numb3<numb4 ${ numb4>numb3}`)
+console.log(`str2<str1  ${str1<str2}`)
+
+console.log(`numb3<=numb4 ${ numb3<=numb4}`)
+console.log(`str2<=str1  ${str1<=str2}`)
+
+
+
+
+
+
+
+
+
