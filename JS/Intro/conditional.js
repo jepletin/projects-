44270026bcc91eee55445765819b23bf4887e534
Lydia  Jepleting <lydiajepleting9@gmail.com>
2026-06-24 10:12:17 +0300

    learning conditional statements

---
+++ JS/Intro/conditional.js
@@ -0,0 +1,37 @@
+//senario 1 
+let age=18;
+if(age){
+    console.log("senario 1 it runs")
+}
+//senario 2
+
+let firstName=""
+if(firstName){
+    console.log("senario 2 it runs")
+
+}
+
+//senario 3
+if (true){
+    console.log("senario 3 it run")
+
+}
+//senario 4
+if (false){
+    console.log("senario 4 it run")
+
+}
+
+//senario 5
+ if (10>20){
+    console.log("senario 5 it run")
+
+}
+
+//senario 6 
+if  (20>10){
+
+    console.log("senario 6 it run")
+
+}
+
---
+++ JS/Intro/thruthyfalsely.js
@@ -0,0 +1,22 @@
+/*Thurthy or falsely
+..>if you know something is not falsely then it is truthy.
+falsely 
+1.false
+2.0
+3.-0
+4.null
+5undefined
+6...>empty string
+7.nan not a number 
+anything that is not fasely is truthy 
+thruthy||falsely..>thruthy
+*/
+
+
+let userName1="lydia jepleting"
+let userName2=""
+let activeUserName=userName2||userName1
+//activeUserName...>
+console.log(activeUserName)
+ let bothUserNameOk=userName1&&userName2
+ console.log(bothUserNameOk)//truthy or falsely
