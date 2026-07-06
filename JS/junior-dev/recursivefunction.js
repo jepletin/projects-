3243d1b281cb59cd73b013d98560df30875eb2a1
Lydia  Jepleting <lydiajepleting9@gmail.com>
2026-07-06 09:35:32 +0300

    recursive functions

--- JS/junior-dev/recursivefunction.js
+++ JS/junior-dev/recursivefunction.js
@@ -1,45 +1,41 @@
  
 
-//  A recursive function is a function thst cslls itself
-// since this creates an endless loop you need  to find a way  to terminate .That is using return statement  and
-//  if <condition>
-// remove the alert 
-// intial promot should be enter your gross salary 
-// any promot after that should be  invalid gross salary  entered .eneter a number  for gross salary .try again 
+// //  A recursive function is a function thst cslls itself
+// // since this creates an endless loop you need  to find a way  to terminate .That is using return statement  and
+// //  if <condition>
+// // remove the alert 
+// // intial promot should be enter your gross salary 
+// // any promot after that should be  invalid gross salary  entered .eneter a number  for gross salary .try again 
 
 
-//  function salaryGrossPromptProcess(){
-//     let gross=prompt("Enter your gross salary");
-//     if (isNaN (gross) ===false){
-//         console.log (`You entered this amount  ${gross} exiting`);
-//         return;
-//     }
-//     // alert(`Invalid gross salary entered .enter a number for gross salary. try again,`);
-//     // salaryGrossPromptProcess();
-//  }
-//  salaryGrossPromptProcess();
-
-
-function salaryGrossPromptProcess(isRetry){
-    let  gross;
-    if (isRetry===false){
-        gross=prompt("Invalid gross salary entered. Enter a number for gross salary. Try again,")
-
-
-    }else{
-        gross=prompt("Enter your gross salary")
-
-
-    }
+ function salaryGrossPromptProcess(){
+    let gross=prompt("Enter your gross salary");
     if (isNaN (gross) ===false){
         console.log (`You entered this amount  ${gross} exiting`);
         return;
     }
-      salaryGrossPromptProcess();
-
-
-}
+    // alert(`Invalid gross salary entered .enter a number for gross salary. try again,`);
+    salaryGrossPromptProcess();
+ }
  salaryGrossPromptProcess();
+
+
+function salaryGrossPromptProcess(){
+    let gross=prompt("Enter your gross salary");
+    if (!isNaN (gross) && gross !== ""){
+        console.log (`You entered this amount  ${gross} exiting`);
+        return;
+    }
+    console.log("Invalid gross salary entered. Enter a number for gross salary. Try again.");
+
+    salaryGrossPromptProcess();
+
+    
+
+    }
+     salaryGrossPromptProcess();
+
+
 
 
  function factorial(n){
@@ -59,3 +55,22 @@ function salaryGrossPromptProcess(isRetry){
         
  }
  console.log(sum(4))
+
+
+ function recursion (x){
+    if (x>0){
+        console.log(x)
+        recursion(x-1)
+    }
+ }
+ recursion(10)
+
+  function factorial (n){
+    if (n===0){
+        return 1;
+
+    }
+       return n* factorial(n-1)
+    
+ }
+ factorial(5)
