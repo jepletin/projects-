ebc5f53dcc2127e94a8ffeeea6cd4ed45c3ad6cb
Lydia  Jepleting <lydiajepleting9@gmail.com>
2026-07-07 09:32:58 +0300

    recursive function

--- JS/junior-dev/basic-engineer/whileloop.js
+++ JS/junior-dev/basic-engineer/whileloop.js
@@ -7,3 +7,108 @@ while(condition){
         condition=false;
     }
 }
+// create a function <any name >
+//the function should take a parameter @param is a number greater than 0;
+//have a loop inside that counts down from this number use while loop it should print n 
+function countDown(N){
+    while(N>0){
+        console.log(N);
+    N--;
+    }
+
+}
+countDown(5);
+
+// create a function @function1 <give it any name>.
+
+// it does not take any parameters
+
+// this function
+
+// 1. prompts a user for the first number
+//    check if number is a valid number greater than 1.
+
+// 2. prompts a user for the second number
+//    check if number is a valid number greater than 1.
+
+// use recursion or a while loop to ensure
+// user enters a correct number for number1 and number2.
+
+// after the numbers are entered call @function2
+// which takes the numbers as parameters.
+
+// example ->
+// @function2(number1, number2)
+
+
+// create a function @function2 <give it any name>.
+
+// this function takes in the following parameters:
+
+// @param1 number greater than 1.
+// @param2 number greater than 1.
+
+// check if @param1 is a number greater than 1.
+// check if @param2 is a number greater than 1.
+
+// example:
+// @function2(3,5)
+
+// create a maths table for it up to
+// i.e. console logs
+
+// ------
+// 3 * 5 =
+// 3 * 4 =
+// 3 * 3 =
+// 3 * 2 =
+// 3 * 1 =
+
+// ------
+
+// 2 * 5 =
+// 2 * 4 =
+// 2 * 3 =
+// 2 * 2 =
+// 2 * 1 =
+
+// ------
+
+// 1 * 5 =
+// 1 * 4 =
+// 1 * 3 =
+// 1 * 2 =
+// 1 * 1 =
+
+
+function mathTable(){
+    let number1;
+    let number2;
+    while(true){
+        number1=Number(prompt("Enter the  first number"))
+        if(!isNaN(number1)&& number1>1){
+            break;
+        }
+        console.log("invalid ,please enter a number bigger  than 1")
+    }
+    while(true){
+        number2=Number(prompt("Enter the  second  number"));
+        if(!isNaN(number2) && number2>1){
+            break;
+        }
+        console.log("invalid ,please enter a number bigger than 1")
+
+
+    }
+    numbers(number1,number2);
+}
+
+function numbers(number1,number2){
+    while (number1<=40) {
+        console.log(`${number1}*${number2} =${number1*number2}`)
+        number1++
+        
+    }
+}
+mathTable();
+//recursive
--- JS/junior-dev/recursivefunction.js
+++ JS/junior-dev/recursivefunction.js
@@ -8,94 +8,141 @@
 // // any promot after that should be  invalid gross salary  entered .eneter a number  for gross salary .try again 
 
 
- function salaryGrossPromptProcess(){
-    let gross=prompt("Enter your gross salary");
-    if (isNaN (gross) ===false){
-        console.log (`You entered this amount  ${gross} exiting`);
-        return;
-    }
-    // alert(`Invalid gross salary entered .enter a number for gross salary. try again,`);
-    salaryGrossPromptProcess();
- }
- salaryGrossPromptProcess();
+//  function salaryGrossPromptProcess(){
+//     let gross=prompt("Enter your gross salary");
+//     if (isNaN (gross) ===false){
+//         console.log (`You entered this amount  ${gross} exiting`);
+//         return;
+//     }
+//     // alert(`Invalid gross salary entered .enter a number for gross salary. try again,`);
+//     salaryGrossPromptProcess();
+//  }
+//  salaryGrossPromptProcess();
 
 
-function salaryGrossPromptProcess(){
-    let gross=prompt("Enter your gross salary");
-    if (!isNaN (gross) && gross !== ""){
-        console.log (`You entered this amount  ${gross} exiting`);
-        return;
-    }
-    console.log("Invalid gross salary entered. Enter a number for gross salary. Try again.");
+// function salaryGrossPromptProcess(){
+//     let gross=prompt("Enter your gross salary");
+//     if (!isNaN (gross) && gross !== ""){
+//         console.log (`You entered this amount  ${gross} exiting`);
+//         return;
+//     }
+//     console.log("Invalid gross salary entered. Enter a number for gross salary. Try again.");
 
-    salaryGrossPromptProcess();
+//     salaryGrossPromptProcess();
 
     
 
-    }
-     salaryGrossPromptProcess();
+//     }
+//      salaryGrossPromptProcess();
 
 
 
 
- function factorial(n){
-    if(n===1){
-        return 1;
-    }
-    return n*factorial (n-1);
+//  function factorial(n){
+//     if(n===1){
+//         return 1;
+//     }
+//     return n*factorial (n-1);
 
- }
- console.log(factorial(5))
+//  }
+//  console.log(factorial(5))
     
- function sum (number){
-    if(number===0){
-        return 1
-    }
-    return number+sum(number-1)
+//  function sum (number){
+//     if(number===0){
+//         return 1
+//     }
+//     return number+sum(number-1)
         
- }
- console.log(sum(4))
+//  }
+//  console.log(sum(4))
 
 
- function recursion (x){
-    if (x>0){
-        console.log(x)
-        recursion(x-1)
-    }
- }
- recursion(10)
+//  function recursion (x){
+//     if (x>0){
+//         console.log(x)
+//         recursion(x-1)
+//     }
+//  }
+//  recursion(10)
 
-  function factorial (n){
-    if (n===0){
-        return 1;
+//   function factorial (n){
+//     if (n===0){
+//         return 1;
 
-    }
-       return n* factorial(n-1)
+//     }
+//        return n* factorial(n-1)
     
- }
- factorial(5)
+//  }
+//  factorial(5)
 
- function factorial2(n=1, lastSolution=1){
-    //end condition
-    //5<=1 false //4<=1 false
-    //3<=1 false
-    //2<=1 false
-    // 1<=1 true
-     if (n<=1){
-        return lastSolution;//120 exit
-     }
+//  function factorial2(n=1, lastSolution=1){
+//     //end condition
+//     //5<=1 false //4<=1 false
+//     //3<=1 false       //(4,5)
+//     //2<=1 false
+//     // 1<=1 true
+//      if (n<=1){
+//         return lastSolution;//120 exit
+//      }
 
-     const newSolution=n*lastSolution;
-     const newN=n-1;//5-1=4//,4-1,=3//,3-1=2// 2-1=1/
+//      const newSolution=n*lastSolution;//20    //3,20  //60//2,60  1,120
+//      const newN=n-1;//5-1=4//,4-1,=3//,3-1=2// 2-1=1/
 
-     //factorial2(4,5)
-     //factorial2(3,20)
-     //factorial2(2,60)
-     //factorial2(1,120)
-     factorial2(newN,newSolution)
+//      //factorial2(4,5)
+//      //factorial2(3,20)
+//      //factorial2(2,60)
+//      //factorial2(1,120)
+//      factorial2(newN,newSolution)
 
- }
-function factorial2(n=1, lastSolution=1){
+//  }
+// function factorial2(n=1, lastSolution=1){
+//     //end condition
+//     //5<=1 false //4<=1 false
+//     //3<=1 false
+//     //2<=1 false
+//     // 1<=1 true
+//      if (n<=1){
+//         return lastSolution;
+//      }
+
+//      const newSolution=n*lastSolution;
+//      const newN=n-1;//5-1=4//,4-1,=3//,3-1=2// 2-1=1/
+
+//      //factorial2(4,5)
+//      //factorial2(3,20)
+//      //factorial2(2,60)
+//      //factorial2(1,120)
+//      return factorial2(newN,newSolution)
+
+//  }
+//  console.log( factorial2(2,3))
+//   console.log( factorial2(2,4))
+//   console.log( factorial2(3,3))
+//   console.log( factorial2(4,3))
+//   console.log( factorial2(3,5))
+//   console.log( factorial2(6,5))
+
+
+
+function frank(n){
+    if(n===0)
+        return 1;
+    return n*frank(n-1)
+//n=6
+//6*5=30
+//30
+//4*30=120
+//3*120=360
+//2*360=720
+//1*720=720
+//n1===0
+
+}
+console.log(frank(6))
+
+
+
+ function factorial2(n, lastSolution=1){
     //end condition
     //5<=1 false //4<=1 false
     //3<=1 false
@@ -104,25 +151,32 @@ function factorial2(n=1, lastSolution=1){
      if (n<=1){
         return lastSolution;
      }
+     
 
-     const newSolution=n*lastSolution;
-     const newN=n-1;//5-1=4//,4-1,=3//,3-1=2// 2-1=1/
+     const newSolution=n*lastSolution;//35
+     const newN=n-1;//6,35
+     //5,1050
+     //4,4200
+     //3,12600
+     //2,25200
+     //1,2520
 
-     //factorial2(4,5)
-     //factorial2(3,20)
-     //factorial2(2,60)
-     //factorial2(1,120)
+
+  //n=7
+  //last=5
      return factorial2(newN,newSolution)
 
  }
- console.log( factorial2(2,3))
-  console.log( factorial2(2,4))
-  console.log( factorial2(3,3))
-  console.log( factorial2(4,3))
-  console.log( factorial2(3,5))
-  console.log( factorial2(6,5))
+ console.log(factorial2(7,5))
+ 
 
-
-
-
-
+ function pow(x, n) {
+  if (n == 1) {
+    return x;
+  } else {
+    return x * pow(x, n - 1);// 8,
+  }
+}
+//2*2
+2
+console.log( pow(4, 3) ); // 8
