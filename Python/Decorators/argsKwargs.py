f665aef937e80cc6948b5329e0db13ed8f0b24c1
Lydia  Jepleting <lydiajepleting9@gmail.com>
2026-09-01 09:16:31 +0300

    learned decorators in python

---
+++ Python/Decorators/argsKwargs.py
@@ -0,0 +1,29 @@
+#kwargs and printing out when a function was called
+
+def log_deco(func):
+    def wrapper(*args, **kwargs):
+        print("------------------------------------------------------------------")
+        print("Args ", args)
+        print("Krwargs ", kwargs)
+        result = func(*args, **kwargs)
+        print(f"the function called was {func._name_}")
+        print("results ", result)
+        print("------------------------------------------------------------------")
+    return wrapper
+
+
+@log_deco
+def hello():
+    print("Hello world")
+    return 123
+
+
+@log_deco
+def sum(a, b):
+    ans = a + b
+    return ans
+
+
+sum(a=20, b=30)  # kwargs
+
+sum(1, 5)  # args
---
+++ Python/Decorators/intro.py
@@ -0,0 +1,38 @@
+"""
+Are powerful tool for
+modifying or extending the behaviour of functions
+or methods without changing their code.
+"""
+"""
+A decorator function should take
+another function as an argument/parameter.
+it should have a wrapper function.
+this function should be able to call the passed function
+
+to use a decorator, you use
+@<decorator function> before function definition
+"""
+
+def my_deco(func):
+    def wrapper():
+        print("Before we call the function")
+        func()
+        print("After we call the function")
+    return wrapper
+
+
+def hello():
+    print("Hello world function executes")
+    print("Hello World")
+
+
+@my_deco
+def french_hello():
+    print("french hello function")
+    print("Bonjour World")
+
+
+# -> french_hello -> my_deco(french_hello) -> wrapper() -> french_hello
+# -> hello() -> hello
+
+french_hello()
