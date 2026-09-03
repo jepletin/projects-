4645236d75cbb78319a8e0b00493ed1f553925dd
Lydia  Jepleting <lydiajepleting9@gmail.com>
2026-09-03 11:28:38 +0300

    completed the bank project by learning getters and setter

--- Python/object_oriented_programming/bank.py
+++ Python/object_oriented_programming/bank.py
@@ -6,27 +6,40 @@ show account.
 getter and setter. →
 → easy to scale function <understing>
 
-------------------------------
-    → Login account
-    → Create account
-    → Deposit
-    → Withdrawal
-    → account balance
-
+→ Login account
+→ Create account
+→ Deposit
+→ Withdrawal
+→ account balance
 """
 
 class BankAccount:
 
-    def __init__(self,name,balance,account_no):
-        self.name=name
-        self.balance=balance
-        self.account_no=account_no
-        
-    #latter change gee
-    def get_balance(self):
-        pass
+    def __init__(self, name, balance, account_no):
+        self.name = name
+        self._balance = balance
+        self.account_no = account_no
 
-    #setter
+    # data i read
+    @property
+    def balance(self):
+        print("somebody tried to read johns balance")
+        return self._balance
+
+    # to control updated
+    @balance.setter
+    def balance(self, value):
+        if not isinstance(value, (int, float)):
+            print("Ensure you pass a number for new balance")
+            return
+
+        if value < 0:
+            print("Ensure new balance must not be less than 0")
+            return
+
+        self._balance = value
+
+    # setter
     def deposit(self):
         pass
 
@@ -36,7 +49,18 @@ class BankAccount:
     def show_account_details(self):
         print(f"Owner {self.name}")
         print(f"Balance {self.balance}")
-        print(f"Account no {self.account_no}")
+        print(f"Account No {self.account_no}")
 
-john=BankAccount(name="lydia jepleting",balance=0,account_no="232323545")
-john.show_account_details()
+
+john = BankAccount(
+    name="John Mwangi",
+    balance=0,
+    account_no="223344223"
+)
+
+print("John balance ", john.balance)
+john.balance=-50000
+# john.show_account_details()
+
+# it is easily accessible →
+# john.balance=4000 setting → updating the property
