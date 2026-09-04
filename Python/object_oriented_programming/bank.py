78865d8838bea21ef3d0fe7845594e88f0db64ac
Lydia  Jepleting <lydiajepleting9@gmail.com>
2026-09-04 10:22:13 +0300

    bank project

--- Python/object_oriented_programming/bank.py
+++ Python/object_oriented_programming/bank.py
@@ -1,24 +1,42 @@
 """
 Abstractions.
-Bank Class→
+Bank class →
 deposits, withdrawal,
-show account.
-getter and setter. →
-→ easy to scale function <understing>
+show account,
+getter and setter.
+→ easy to scale function <understanding>
 
-→ Login account
-→ Create account
-→ Deposit
-→ Withdrawal
-→ account balance
+    → Login account
+    → Create account
+    → Deposit
+    → Withdrawal
+        → account balance
+
+"""
+
+"""
+Static (it does not change). <class properties> properties. <Belong to the class>
+Static Method <>. Class method. <Function belongs to the class>
+
+Why would you want to use a class property?
 """
 
 class BankAccount:
+    clients = 0
+    bank_name = "Post Bank"  # static property
 
+    # this →
     def __init__(self, name, balance, account_no):
         self.name = name
         self._balance = balance
         self.account_no = account_no
+
+        # BankAccount.clients = BankAccount.clients + 1
+        # self.__class__.clients = self.__class__.clients + 1
+        BankAccount.add_client()
+        # self.__class__.bank_name = name_bank
+        # self.__class__.clients += 1
+        # BankAccount.clients += self.clients
 
     # data i read
     @property
@@ -39,7 +57,7 @@ class BankAccount:
 
         self._balance = value
 
-    # setter
+    # instance method self -> object
     def deposit(self):
         pass
 
@@ -51,6 +69,31 @@ class BankAccount:
         print(f"Balance {self.balance}")
         print(f"Account No {self.account_no}")
 
+    # -------------------------
+    # Static Method. <class method(cls)> @staticmethod →
+    @staticmethod
+    def calculate_interest(amount, year):
+        rate = 10
+        interest_per_year = amount * (rate / 100)
+        interest_total = interest_per_year * year
+        total = amount + interest_total
+
+        print(
+            f"If you take a loan of ${amount}, "
+            f"interest rate per year {interest_per_year}"
+        )
+        print(
+            f"Total interest {interest_total}, "
+            f"total to pay {total} after {year}"
+        )
+
+    # -------------------------
+    # Class Method. <>
+    # class itself.
+    @classmethod
+    def add_client(cls):
+        cls.clients = cls.clients + 1
+
 
 john = BankAccount(
     name="John Mwangi",
@@ -58,9 +101,14 @@ john = BankAccount(
     account_no="223344223"
 )
 
-print("John balance ", john.balance)
-john.balance=-50000
-# john.show_account_details()
+print("Total clients", BankAccount.clients)
 
-# it is easily accessible →
-# john.balance=4000 setting → updating the property
+samuel = BankAccount(
+    name="Samuel",
+    balance=0,
+    account_no="223344223"
+)
+
+print("Total clients", BankAccount.clients)
+
+BankAccount.calculate_interest(50000, 3)
